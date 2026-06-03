5b2dc18dcf348cc31c8a511543fd313afe3c7f23
bekzod <raynbekzod@gmail.com>
2026-06-03 13:19:22 +0300

    update

---
+++ dars 4/main.js
@@ -0,0 +1,38 @@
+let string = ""
+
+let number = 456442
+
+let boolean = true | false
+
+console.log(null);
+
+let salon
+
+let telefonlarruyhati = [
+    
+    {name:"iphone", model:15, price:15000},
+]
+
+let object = {name:"iphone", model:15, price:15000};
+
+document.write("Assalom")
+
+console.log(telefonlarruyhati);
+
+alert(telefonlarruyhati);
+
+console.log(5+5);
+
+console.log("5"+10);
+
+console.log(2*2);
+
+console.log(4/2);
+
+console.log(7%2);
+
+console.log(5**3);
+
+let a = 20;
+
+let b ="20";
