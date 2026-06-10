95632c76a4f1d9b1e4c96ef5e515b2282d8bcde0
bekzod <raynbekzod@gmail.com>
2026-06-10 14:12:21 +0300

    update

--- dars 7/main.js
+++ dars 7/main.js
@@ -1,11 +1,12 @@
-let son = prompt("Son kiriting:");
+let son =+ prompt("Son kiriting");
 
-if (son % 3 === 0 && son % 5 === 0) {
-    console.log("FIZZBUZZ");
-} else if (son % 3 === 0) {
-    console.log("FIZZ");
-} else if (son % 5 === 0) {
-    console.log("BUZZ");
-} else {
-    console.log("Hech biriga bo'linmaydi");
-}
+console.log(son);
+console.log(Math,floor(son),'floor');
+console.log(Math,ceil(son),'ceil');
+console.log(Math,rounds(son),'rounds');
+console.log(Math,abs(son),'abs');
+console.log(Math,max(3,2,3231,32,3,312,323,13,2,13,21313,123,23,31,23,13123,312,312),'abs');
+console.log(Math,min(3,2,3231,32,3,312,323,13,2,13,21313,123,23,31,23,13123,312,312),'abs');
+console.log(Math,pow(4,8),'daraja X Y ');
+console.log(Math,sqrt(64),'sprt');
+console.log(Math,rounds(son),'rounds');
---
+++ dars 8/main.js
@@ -0,0 +1,49 @@
+let car = {
+    brand:" BMW",
+    madel: "BMW M5 F90 COMPETITION",
+    color: "qora",
+    weight: "1,970 kg",
+    price: "$120,000",
+    horsepower: "617 ",
+    max_speed: "305 km/h",
+}
+console.log("salom hush  kelib ha albata bizda", car.model, "bor", "bu mashina", car.color, "rangda va", car.weight, "og'irlikda",);
+
+let car2 = {
+    brand:" Audi",
+    madel: "Audi A8",
+    color: "oq",
+    weight: "1,900 kg",
+    price: "$90,000",
+    horsepower: "335 ",
+    max_speed: "250 km/h",
+}
+console.log("salom hush  kelib ha albata bizda", car2.model, "bor", "bu mashina", car2.color, "rangda va", car2.weight, "og'irlikda",);
+
+
+
+let football = {
+    name: "Cristiano Ronaldo",
+    age: 39,
+    nationality: "Portuguese",
+    club: "Al Nassr",
+    position: "Forward",
+    goals: 973 ,
+    assists: 200,
+}
+console.log("salom ha albata bizda", football.name, "haqida inf bor", "bu futbolchi", football.nationality,"u", football.club, "da oynidi", "hozirgi damda u", football.goals, "gool la urgan",  football.age, "yoshda",);
+
+let football2 = {
+    name: "Lionel Messi",
+    age: 36,
+    nationality: "Argentine",
+    club: "Paris Saint-Germain",
+    position: "Forward",
+    goals: 750 ,
+    assists: 300,
+}
+console.log("salom ha albata bizda", football2.name, "haqida inf bor", "bu futbolchi", football2.nationality,"u", football2.club, "da oynidi", "hozirgi damda u", football2.goals, "gool la urgan",  football2.age, "yoshda",);
+
+
+
+
