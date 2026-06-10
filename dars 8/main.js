9e291c70151fff63107efdebed05da1db0611cd4
bekzod <raynbekzod@gmail.com>
2026-06-10 14:22:31 +0300

    update

--- dars 8/main.js
+++ dars 8/main.js
@@ -1,26 +1,24 @@
-let car = {
-    brand:" BMW",
-    madel: "BMW M5 F90 COMPETITION",
-    color: "qora",
-    weight: "1,970 kg",
-    price: "$120,000",
-    horsepower: "617 ",
-    max_speed: "305 km/h",
-}
-console.log("salom hush  kelib ha albata bizda", car.model, "bor", "bu mashina", car.color, "rangda va", car.weight, "og'irlikda",);
+// let car = {
+//     brand:" BMW",
+//     madel: "BMW M5 F90 COMPETITION",
+//     color: "qora",
+//     weight: "1,970 kg",
+//     price: "$120,000",
+//     horsepower: "617 ",
+//     max_speed: "305 km/h",
+// }
+// console.log("salom hush  kelib ha albata bizda", car.model, "bor", "bu mashina", car.color, "rangda va", car.weight, "og'irlikda",);
 
-let car2 = {
-    brand:" Audi",
-    madel: "Audi A8",
-    color: "oq",
-    weight: "1,900 kg",
-    price: "$90,000",
-    horsepower: "335 ",
-    max_speed: "250 km/h",
-}
-console.log("salom hush  kelib ha albata bizda", car2.model, "bor", "bu mashina", car2.color, "rangda va", car2.weight, "og'irlikda",);
-
-
+// let car2 = {
+//     brand:" Audi",
+//     madel: "Audi A8",
+//     color: "oq",
+//     weight: "1,900 kg",
+//     price: "$90,000",
+//     horsepower: "335 ",
+//     max_speed: "250 km/h",
+// }
+// console.log("salom hush  kelib ha albata bizda", car2.model, "bor", "bu mashina", car2.color, "rangda va", car2.weight, "og'irlikda",);
 
 let football = {
     name: "Cristiano Ronaldo",
@@ -43,7 +41,3 @@ let football2 = {
     assists: 300,
 }
 console.log("salom ha albata bizda", football2.name, "haqida inf bor", "bu futbolchi", football2.nationality,"u", football2.club, "da oynidi", "hozirgi damda u", football2.goals, "gool la urgan",  football2.age, "yoshda",);
-
-
-
-
